65149b79fa16f5875628fd35febdb05c20d4fc07
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2023-11-08 11:59:10 +0530

    FINAL

--- Devsearch/urls.py
+++ Devsearch/urls.py
@@ -19,7 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
+
+admin.site.site_header="DevCode Admin"
+admin.site.site_title="DevCode Admin Portal"
+admin.site.index_title="DevCode"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
